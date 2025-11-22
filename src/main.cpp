a03a1043ca4b893a6541ebd0d543a4807081041f
Jan <laitenberger2018@gmail.com>
2025-11-22 17:32:05 +0100

    Add pattern "xmasRedGreen"

--- src/main.cpp
+++ src/main.cpp
@@ -42,21 +42,58 @@ void loop() {
 #define NUM_LEDS    5
 CRGB leds[NUM_LEDS];
 
+enum LedStateEnum {
+  ST_INIT,
+  ST_IDLE,
+  ST_FADEIN,
+  ST_HOLD,
+  ST_FADEOUT
+};
+
+struct LedState {
+  LedStateEnum state;
+  uint16_t waitCounter;      // Wartezeit bis Start
+  uint8_t  fadeCounter;      // 0–255 Fadewert
+  uint8_t  fadeMin;
+  uint8_t  fadeMax;
+  CRGB     targetColor;
+  uint32_t holdMillis;
+  bool debugPrinted;         // verhindert Spam im Serial
+};
+
+LedState ledState[NUM_LEDS];
+
 #define BRIGHTNESS          20
 #define FRAMES_PER_SECOND  120
+
+void initLedStates() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    ledState[i].state        = ST_INIT;
+    ledState[i].debugPrinted = false;
+  }
+}
 
 void setup() {
   delay(3000); // 3 second delay for recovery
   
+  Serial.begin(115200);
+  Serial.println("ESP32C3 setup...");
+
   // tell FastLED about the LED strip configuration
   FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 
   // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
+
+  // initialize LED states
+  initLedStates();
+
+  Serial.println("setup done.");
 }
 
 // Forward declarations for pattern functions
+void xmasRedGreen();
 void rainbow();
 void rainbowWithGlitter();
 void confetti();
@@ -68,7 +105,9 @@ void addGlitter(fract8 chanceOfGlitter);
 
 // List of patterns to cycle through.  Each is defined as a separate function below.
 typedef void (*SimplePatternList[])();
-SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
+//SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
+
+SimplePatternList gPatterns = { xmasRedGreen };
 
 uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
@@ -94,6 +133,89 @@ void nextPattern()
 {
   // add one to the current pattern number, and wrap around at the end
   gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+}
+
+void xmasRedGreen() 
+{
+  for(int pos = 0; pos < NUM_LEDS; pos++)
+  {
+    LedState &ls = ledState[pos];
+    switch (ls.state)
+    {
+    // ---------------------------------------------------------
+    case ST_INIT:
+      ls.waitCounter = random8(10, 50) * FRAMES_PER_SECOND/2;
+      ls.state = ST_IDLE;
+      ls.debugPrinted = false;
+      Serial.printf("LED %d -> ST_IDLE (wait=%d)\n", pos, ls.waitCounter);
+      break;
+    // ---------------------------------------------------------
+    case ST_IDLE:
+      if (ls.waitCounter > 0) {
+        ls.waitCounter--;
+        break;
+      }
+      // Zeit zum starten: Farbe wählen
+      if(random8() & 1) {
+        ls.targetColor = CRGB::Red;
+        ls.fadeMax = 255;
+      } else {
+        ls.targetColor = CRGB::Green;
+        ls.fadeMax = 185; // Grünton etwas dunkler, damit er etwa gleich hell wie rot wirkt
+      }
+      ls.fadeMin     = 0;
+      ls.fadeCounter = ls.fadeMin;
+      ls.state = ST_FADEIN;
+      ls.debugPrinted = false;
+      Serial.printf("LED %d -> ST_FADEIN (max=%d)\n", pos, ls.fadeMax);
+      break;
+    // ---------------------------------------------------------
+    case ST_FADEIN:
+      if (!ls.debugPrinted) {
+        Serial.printf("LED %d start FADEIN\n", pos);
+        ls.debugPrinted = true;
+      }
+      leds[pos] = ls.targetColor;
+      leds[pos].nscale8(ls.fadeCounter);
+      if (++ls.fadeCounter >= ls.fadeMax) {
+        ls.state = ST_HOLD;
+        ls.holdMillis = millis();
+        ls.debugPrinted = false;
+        Serial.printf("LED %d -> ST_HOLD\n", pos);
+      }
+      break;
+    // ---------------------------------------------------------
+    case ST_HOLD:
+      if (!ls.debugPrinted) {
+        Serial.printf("LED %d holding\n", pos);
+        ls.debugPrinted = true;
+      }
+      // halte 200–600 ms
+      if (millis() - ls.holdMillis > random16(200, 600)) {
+        ls.fadeCounter = ls.fadeMax;
+        ls.state = ST_FADEOUT;
+        ls.debugPrinted = false;
+        Serial.printf("LED %d -> ST_FADEOUT\n", pos);
+      }
+      break;
+    // ---------------------------------------------------------
+    case ST_FADEOUT:
+      if (!ls.debugPrinted) {
+        Serial.printf("LED %d start FADEOUT\n", pos);
+        ls.debugPrinted = true;
+      }
+      leds[pos] = ls.targetColor;
+      leds[pos].nscale8(ls.fadeCounter);
+      if (ls.fadeCounter > 0)
+        ls.fadeCounter--;
+      else {
+        ls.state = ST_INIT;  // neu starten
+        ls.debugPrinted = false;
+        Serial.printf("LED %d -> ST_INIT\n", pos);
+      }
+      break;
+    }
+  }
 }
 
 void rainbow() 
